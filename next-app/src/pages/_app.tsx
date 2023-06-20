068e36afa219db92e8bda7fc9bb1790a753c4dac
96kuroguro <96kuroguro@gmail.com>
2023-06-21 00:32:26 +0900

    layout調整

--- next-app/src/pages/_app.tsx
+++ next-app/src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from './layout/admin';
+import Layout from './admin/components/layout';
+import theme from '../theme';
+import { ThemeProvider } from '@mui/material/styles';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return ( 
+    <ThemeProvider theme={theme}>
+
     <Layout>
       <Component {...pageProps} />
     </Layout>
+
+    </ThemeProvider>
+
   );
 }
